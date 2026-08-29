a81d57b76d4838bd76bf672eb51581586697c575
Theo Thomas <214850525+Joyboy-World@users.noreply.github.com>
2026-08-29 11:10:46 -0400

    M1 Critical Thinking — Option #2: RestaurantInfo

---
+++ M1-CriticalThinking/src/RestaurantInfo.java
@@ -0,0 +1,12 @@
+public class RestaurantInfo {
+
+    public static void main(String[] args) {
+        // M1 Critical Thinking, Option #2
+        // name and address, one item per line.
+        System.out.println("Painters' Restaurant");
+        System.out.println("416 South Country Road");
+        System.out.println("Brookhaven");
+        System.out.println("New York");
+        System.out.println("11719");
+    }
+}
